98948e4b438d155bbea3866e3f1580c746e32788
wyh <251793636@qq.com>
2024-11-03 02:18:37 +0800

    3

--- vite-project/src/router/index.ts
+++ vite-project/src/router/index.ts
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventListView from '@/views/EventListView.vue'
 import AboutView from '@/views/AboutView.vue'
-import EventDetailView from '@/views/EventDetailView.vue'
+import EventDetailView from '@/views/event/DetailView.vue'
+import EventRegisterView from '@/views/event/RegisterView.vue'
+import EventEditView from '@/views/event/EditView.vue'
+import EventLayoutView from '@/views/event/LayoutView.vue'
 import StudentsView from '@/views/StudentView.vue'
 
 const router = createRouter({
@@ -15,10 +18,31 @@ const router = createRouter({
     },
     {
       path: '/event/:id',
-      name: 'event-detail-view',
-      component: EventDetailView,
-      props: true,
+      name: 'event-layout-view',
+            component: EventLayoutView,
+            props: true,
+            children: [
+              {
+                path: '',
+                name: 'event-detail-view',
+                component: EventDetailView,
+                props: true
+             },
+              {
+                path: 'register',
+                name: 'event-register-view',
+                component: EventRegisterView,
+                props: true
+              },
+              {
+                path: 'edit',
+                name: 'event-edit-view',
+                component: EventEditView,
+                props: true
+              }
+            ]
     },
+    
     {
       path: '/about',
       name: 'about',
---
+++ vite-project/src/views/event/RegisterView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { type Event } from '@/types'
+const props = defineProps<{
+  event: Event
+  id: string
+}>()
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const { event } = toRefs(props)
+</script>
+<template>
+  <p>Register event here</p>
+</template>
